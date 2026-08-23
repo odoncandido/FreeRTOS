11f7b3d78d7e37756d0f7fe00d23a44867d2513d
Odon Candido <odon@kali.odon>
2026-08-23 22:37:57 +0200

    Task

---
+++ ESP_IDF/Tasks/main/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "math.h"
+
+void task_n1(void * params){
+	while(true){
+
+	}
+}
+void task_n2(void * params){
+	while(pdTRUE){
+
+	}
+
+}
+void task_n3(void * params){
+
+}
+void task_n4(void * params){
+
+}
+void task_n5(void * params){
+
+}
+
+void app_main(void)
+{
+
+}
